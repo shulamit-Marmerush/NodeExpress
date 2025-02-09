554c4f0f83e3ea04c366500c16bd0ff71f3cb84d
Shulamit-Marmerush <sm6736243@gmail.com>
2025-02-09 12:20:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Endpoint לקבלת רשימת האפליקציות
-app.get('/services', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         const apiKey = 'rnd_RSUH32z2N8gBuccUJFEPVLah3l2P'; // הכנס את ה-API Key שלך כאן
         const response = await axios.get('https://api.render.com/v1/services', {
